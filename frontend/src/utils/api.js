93ffc4d776aca896ab9087de88bea7bdad0ddc19
apjgaming <lewiand1234@gmail.com>
2025-07-14 11:41:12 -0600

    backend url update for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://healthy-gaming-timer.onrender.com/',
 });
 
 api.interceptors.request.use((config) => {
